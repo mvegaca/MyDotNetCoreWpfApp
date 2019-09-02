40f1a47cde192a5cfbca8c53014e1e05e583cb82
Martin Vega <mvegaca@outlook.com>
2019-09-02 11:53:57 +0200

    Set Mahapps HamburgerMenu as shell navigation

--- MyDotNetCoreWpfPrismApp/ViewModels/ShellWindowViewModel.cs
+++ MyDotNetCoreWpfPrismApp/ViewModels/ShellWindowViewModel.cs
@@ -1,6 +1,4 @@
-﻿using System;
-using System.Collections.ObjectModel;
-using System.Linq;
+﻿using System.Collections.ObjectModel;
 using MahApps.Metro.Controls;
 using Prism.Commands;
 using Prism.Mvvm;
@@ -29,46 +27,18 @@ namespace MyDotNetCoreWpfPrismApp.ViewModels
 
         public DelegateCommand<string> NavigateCommand { get; private set; }
 
-        public DelegateCommand GoBackCommand { get; private set; }
-
         public ShellWindowViewModel(IRegionManager regionManager)
         {
             _regionManager = regionManager;            
             MenuItemInvokedCommand = new DelegateCommand(OnMenuItemInvoked);
             NavigateCommand = new DelegateCommand<string>(Navigate);
-            GoBackCommand = new DelegateCommand(OnGoBack, CanGoBack);
         }
-
-        internal void Initialize()
-        {
-            //_regionManager.Regions["MainRegion"].NavigationService.Navigated += OnNavigated;
-        }
-
-        private void OnNavigated(object? sender, RegionNavigationEventArgs e)
-            => GoBackCommand.RaiseCanExecuteChanged();
 
         private void Navigate(string navigationPath)
             => _regionManager.RequestNavigate("MainRegion", navigationPath, NavigationComplete);
 
         private void OnMenuItemInvoked()
-            => _regionManager.RequestNavigate("MainRegion", SelectedMenuItem.Tag.ToString(), NavigationComplete);
-
-        private bool CanGoBack()
-        {
-            if (_regionManager.Regions.Any())
-            {
-                var mainRegion = _regionManager.Regions["MainRegion"];
-                if (mainRegion != null)
-                {
-                    return mainRegion.NavigationService.Journal.CanGoBack;
-                }
-            }            
-
-            return false;
-        }
-
-        private void OnGoBack()
-            => _regionManager.Regions["MainRegion"].NavigationService.Journal.GoBack();
+            => _regionManager.RequestNavigate("MainRegion", SelectedMenuItem.Tag.ToString(), NavigationComplete);        
 
         private void NavigationComplete(NavigationResult obj)
         {
--- MyDotNetCoreWpfPrismApp/Views/ShellWindow.xaml.cs
+++ MyDotNetCoreWpfPrismApp/Views/ShellWindow.xaml.cs
@@ -1,30 +1,12 @@
-﻿using System;
-using System.Windows;
-using MahApps.Metro.Controls;
-using MyDotNetCoreWpfPrismApp.ViewModels;
-using Prism.Regions;
+﻿using MahApps.Metro.Controls;
 
 namespace MyDotNetCoreWpfPrismApp.Views
 {
     public partial class ShellWindow : MetroWindow
     {
-        private IRegionManager _regionManager;
-        public ShellWindow(IRegionManager regionManager)
+        public ShellWindow()
         {
-            _regionManager = regionManager;
             InitializeComponent();
-        }
-
-        private void ListView_SelectionChanged(object sender, System.Windows.Controls.SelectionChangedEventArgs e)
-        {
-            var option = (HamburgerMenuGlyphItem)e.AddedItems[0];
-            var viewName = (string)option.Tag;
-            _regionManager.RequestNavigate("MainRegion", viewName);
-        }
-
-        private void OnLoaded(object sender, RoutedEventArgs e)
-        {
-            ((ShellWindowViewModel)DataContext).Initialize();
         }
     }
 }
